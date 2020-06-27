77d9096b6defe6e5eebaf01725f702b4ac4e8281
475Fatema <66346391+475Fatema@users.noreply.github.com>
2020-06-27 21:12:16 +0300

    تم الحل

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,26 +1,38 @@
 // السؤال الأول
 // 1
+const name = "fatema";
 
 // 2
+let number = 18 ;
 
 // 3 and 4
+console.log=[name +"  "+  number ]
 
 // 5 and 6
-
+console.log (number*5 )
+number=number*3
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+     let hobbies=["photography,cooking,drawing, reading"]
 
-// 2
+// 2 
+     console.log(hobbies)
+    
 
-// 3
+// 3  
+     hobbies.pop( );
+     console.log( hobbies);
 
-// 4
+// 4  
+       let fatema ={ track : "web" , lang : "css"    };
 
 // 5
+   Object.ta=["rahaf" ]
 
-// 6
+// 6 
+      console.log (fatema);
 
 // 7
 
